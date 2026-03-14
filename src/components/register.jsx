2d7e07312b61d3346a9817cbcf2003b8c0398a01
Nagarajan-2003 <nagarajanmnagarajan77@gmail.com>
2026-03-14 17:44:19 +0530

    Fix: Import Link in register.jsx

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './login.css'; 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({ username: '', password: '', password2: '' });
